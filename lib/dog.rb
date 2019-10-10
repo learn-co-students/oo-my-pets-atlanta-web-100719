cefa1671c50b9d3f86fe195eee775544c8ada8f6
clarkj99 <clark@stontech.net>
2019-10-09 23:33:44 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,25 @@
 class Dog
-  # code goes here
+  attr_accessor :owner, :mood
+  attr_reader :name
+  @@all = []
+
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+    owner.dogs << self
+    save
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def buy_dog(name)
+    Dog.new(name, self)
+  end
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,76 @@
+
+require "pry"
+
 class Owner
-  # code goes here
+  attr_accessor :cats, :dogs
+  attr_reader :name, :species
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @cats = []
+    @dogs = []
+    save
+  end
+
+  def save
+    @@all << self
+  end
+
+  def say_species
+    "I am a #{@species}."
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@all.count
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+  def buy_cat(name)
+    # binding pry
+    Cat.new(name, self)
+  end
+
+  def buy_dog(name)
+    Dog.new(name, self)
+  end
+
+  def change_moods(pets, mood)
+    pets.each do |pet|
+      pet.mood = mood
+    end
+  end
+
+  def remove_pets(pets, mood)
+    pets.each do |pet|
+      pet.mood = mood
+      pet.owner = nil
+    end
+    pets.clear
+  end
+
+  def walk_dogs
+    change_moods(@dogs, "happy")
+  end
+
+  def feed_cats
+    change_moods(@cats, "happy")
+  end
+
+  def sell_pets
+    remove_pets(@dogs, "nervous")
+    remove_pets(@cats, "nervous")
+  end
+
+  def list_pets
+    "I have #{@dogs.count} dog(s), and #{@cats.count} cat(s)."
+  end
 end
